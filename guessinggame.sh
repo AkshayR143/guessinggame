58c3499f91c9bc2f081f05b68c4a47bff4372848
AkshayR143 <108896863+AkshayR143@users.noreply.github.com>
2022-07-20 14:41:03 +0530

    Update guessinggame.sh
    .

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,21 @@
-function Guessthecount {
-
-local cnt=$(ls . | wc -w) 
-local compare=1
-local ans=0
-
-while [[ $compare -eq 1 ]]
-do
+function Guessthecount 
+{
+   local cnt=$(ls . | wc -w) 
+   local compare=1
+   local ans=0
+   while [[ $compare -eq 1 ]]
+   do
 	echo "how many files are there in this folder?"
 	read ans
 	if [[ $ans -eq $cnt ]]
-		
-		then compare=0
+	then compare=0
 		echo "you have guessed correct number of files! :$ans"
 	elif [[ $ans -lt $cnt ]]
-		then
+	then
 		echo "Sorry! Your guess is wrong and it is too high"
 	else
 		echo "Sorry! Your guess is wrong and it is too low"
 	fi
 done
-
-
 }
-
 Guessthecount
